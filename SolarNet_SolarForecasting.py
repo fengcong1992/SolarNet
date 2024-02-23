b053bd7057569c1c7cce0fb2ffba48a3593fbd03
Adrian Buntschu <>
2024-02-23 13:16:01 +0100

    Adapt GPU code for newer tensorflow version

--- SolarNet_SolarForecasting.py
+++ SolarNet_SolarForecasting.py
@@ -41,11 +41,12 @@ end_train = pd.to_datetime('2014-12-31')
 end_validation = pd.to_datetime('2015-12-31')
 end_test = pd.to_datetime('2017-12-31')
 
-file_name = 'Data/Data_3Days.pkl'
+file_name = 'Data_3Days.pkl'
 df_train = pd.read_pickle(file_name)
 df_validation = pd.read_pickle(file_name)
 df_test = pd.read_pickle(file_name)
-print(df_train.head())
+
+print(df_train)
 
 params1 = {'batch_size': train_batchsize,
            'dim': (img_size, img_size, 3 * no_img),
@@ -78,16 +79,16 @@ model.summary()
 conv_base.summary()
 
 # compile model
-optimizer = optimizers.Adam(learning_rate=0.00001, beta_1=0.9, beta_2=0.999, epsilon=None, weight_decay=0.0, amsgrad=False)
+optimizer = optimizers.Adam(learning_rate=0.00001, beta_1=0.9, beta_2=0.999, weight_decay=0.0, amsgrad=False)
 class MyModelCheckPoint(ModelCheckpoint):
     def __init__(self, singlemodel, *args, **kwargs):
         self.singlemodel = singlemodel
         super(MyModelCheckPoint, self).__init__(*args, **kwargs)
 
-    def on_epoch_end(self, epoch, logs=None):
+    def on_epoch_end(self, epoch, logs=1):
         self.model = self.singlemodel
         super(MyModelCheckPoint, self).on_epoch_end(epoch, logs)
-checkpointer1 = MyModelCheckPoint(model, filepath="BestSingleModel.hdf5", save_best_only=True, verbose=1)
+checkpointer1 = MyModelCheckPoint(model, filepath="BestSingleModel.keras", save_best_only=True, verbose=1)
 
 
 model.compile(loss='mean_absolute_error', optimizer=optimizer, metrics=['mae'])
@@ -98,26 +99,24 @@ print('n devices: %i' % strategy.num_replicas_in_sync)
 with strategy.scope():
     parallel_model = model
 
-history = parallel_model.fit_generator(train_generator,
+history = parallel_model.fit(train_generator,
                                        steps_per_epoch=int(df_train.shape[0] / train_batchsize),
                                        epochs=60,
                                        validation_data=validation_generator,
                                        validation_steps=int(df_validation.shape[0] / validation_batchsize),
                                        callbacks=[checkpointer1],
-                                       verbose=2,
-                                       workers=6,
-                                       use_multiprocessing=False)
-model.save(os.path.join('Last_SingleModel.hdf5'))
+                                       verbose=2)
+model.save(os.path.join('Last_SingleModel.keras'))
 
 # predict
 try:
-    model.load_weights(os.path.join('BestSingleModel.hdf5'))
+    model.load_weights(os.path.join('BestSingleModel.keras'))
     print('Succss in loading the best single model')
 except:
     print('Fail to load the best single model')
     pass
 
-y_hat = model.predict_generator(generator=test_generator, steps=df_test.shape[0] / test_batchsize)
+y_hat = model.predict(test_generator, steps=df_test.shape[0] / test_batchsize)
 
-
+print(y_hat)
 
